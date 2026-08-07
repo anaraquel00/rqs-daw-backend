db8314c41527882f0e78248e8915d10bddbbe750
Ana Raquel de Holanda Barros <anaraquel00@gmail.com>
2026-08-07 09:55:58 -0300

    fix server.js

--- server.js
+++ server.js
@@ -57,5 +57,3 @@ app.listen(PORT, '0.0.0.0', () => {
     console.log(`[RQS MAINFRAME] Módulos operacionais: [DSP] [MIXER] [VIDEO] [STEMS]`);
 });
 
-server.keepAliveTimeout = 120000;
-server.headersTimeout = 120000;
